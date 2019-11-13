ac30d3c3244867772f3357ca96b729376e7a88b1
emard <vordah@gmail.com>
2019-11-13 14:08:29 +0100

    move line "must_write" after must_erase check

--- ecp5.py
+++ ecp5.py
@@ -524,10 +524,10 @@ class ecp5:
       if file_block:
         flash_block = self.flash_fast_read_block(addr=addr+bytes_uploaded, length=len(file_block))
         must_erase = False
-        must_write = False # TODO must_write[i] for each 256 byte block
         for i in range(len(file_block)):
           if (flash_block[i] & file_block[i]) != file_block[i]:
             must_erase = True
+        must_write = False # TODO must_write[i] for each 256 byte block
         if must_erase: # erase will reset all bytes to 0xFF
           for i in range(len(file_block)):
             if file_block[i] != 0xFF:

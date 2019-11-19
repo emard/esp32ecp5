a9bffc81fd1aab2b1337b8fa3629fa79888d7d67
emard <vordah@gmail.com>
2019-11-19 02:25:07 +0100

    comment out unused code in order not to compile in and waste RAM

--- ecp5.py
+++ ecp5.py
@@ -386,26 +386,26 @@ class ecp5:
 
   # 256-byte write block is too short for hardware SPI to accelerate
   # flash_fast_write_block() is actually slower than flash_write_block()
-  def flash_fast_write_block(self, block, addr=0):
-    self.sdr0(b"\x60") # SPI WRITE ENABLE
-    self.send_tms(0) # -> capture DR
-    self.send_tms(0) # -> shift DR
-    # self.bitreverse(0x40) = 0x02 -> 0x02000000
-    # send bits of 0x02 before the TCK glitch
-    self.send_data_byte_reverse(0x02,0,7) # LSB bit 0 not sent now
-    a = pack(">I", addr)
-    self.hwspi.init(sck=Pin(self.gpio_tck)) # 1 TCK-glitch TDO=0 as LSB bit
-    self.hwspi.write(a[1:4]) # send 3-byte address
-    self.hwspi.write(block[:-1]) # whole block except last byte
-    # switch from SPI to bitbanging mode
-    self.hwspi.init(sck=Pin(self.gpio_dummy)) # avoid TCK-glitch
-    self.bitbang_jtag_on()
-    self.send_data_byte_reverse(block[-1],1,8) # last byte -> exit 1 DR
-    self.send_tms(0) # -> pause DR
-    self.send_tms(1) # -> exit 2 DR
-    self.send_tms(1) # -> update DR
-    self.send_tms(1) # -> select DR scan
-    self.flash_wait_status()
+#  def flash_fast_write_block(self, block, addr=0):
+#    self.sdr0(b"\x60") # SPI WRITE ENABLE
+#    self.send_tms(0) # -> capture DR
+#    self.send_tms(0) # -> shift DR
+#    # self.bitreverse(0x40) = 0x02 -> 0x02000000
+#    # send bits of 0x02 before the TCK glitch
+#    self.send_data_byte_reverse(0x02,0,7) # LSB bit 0 not sent now
+#    a = pack(">I", addr)
+#    self.hwspi.init(sck=Pin(self.gpio_tck)) # 1 TCK-glitch TDO=0 as LSB bit
+#    self.hwspi.write(a[1:4]) # send 3-byte address
+#    self.hwspi.write(block[:-1]) # whole block except last byte
+#    # switch from SPI to bitbanging mode
+#    self.hwspi.init(sck=Pin(self.gpio_dummy)) # avoid TCK-glitch
+#    self.bitbang_jtag_on()
+#    self.send_data_byte_reverse(block[-1],1,8) # last byte -> exit 1 DR
+#    self.send_tms(0) # -> pause DR
+#    self.send_tms(1) # -> exit 2 DR
+#    self.send_tms(1) # -> update DR
+#    self.send_tms(1) # -> select DR scan
+#    self.flash_wait_status()
 
   # data is bytearray of to-be-read length
 #  def flash_read_block(self, data, addr=0):

470cd1ff078ade154b89478ad08af28189724dc5
emard <vordah@gmail.com>
2020-09-04 23:26:30 +0200

    comment reverse bits table

--- rbp/artix7.py
+++ rbp/artix7.py
@@ -77,7 +77,8 @@ class artix7:
     self.flash_erase_size = const(65536) # WROVER
     flash_erase_cmd = { 4096:0x20, 32768:0x52, 65536:0xD8 } # erase commands from FLASH PDF
     self.flash_erase_cmd = flash_erase_cmd[self.flash_erase_size]
-    self.rb=bytearray(256) # reverse bits self.init_reverse_bits()
+    #self.rb=bytearray(256) # reverse bits
+    #self.init_reverse_bits()
     self.spi_channel = const(2) # -1 soft, 1:sd, 2:jtag
     self.init_pinout_jtag()
     self.dummy4=bytearray(4)
@@ -92,18 +93,18 @@ class artix7:
   #    print("%02X" % block[len(block)-n-1], end="")
   #  print(tail, end="")
 
-  @micropython.viper
-  def init_reverse_bits(self):
-    p8rb=ptr8(addressof(self.rb))
-    #p8rb=memoryview(self.rb)
-    for i in range(256):
-      v=i
-      r=0
-      for j in range(8):
-        r<<=1
-        r|=v&1
-        v>>=1
-      p8rb[i]=r
+  #@micropython.viper
+  #def init_reverse_bits(self):
+  #  p8rb=ptr8(addressof(self.rb))
+  #  #p8rb=memoryview(self.rb)
+  #  for i in range(256):
+  #    v=i
+  #    r=0
+  #    for j in range(8):
+  #      r<<=1
+  #      r|=v&1
+  #      v>>=1
+  #    p8rb[i]=r
 
   @micropython.viper
   def send_tms(self, tms:int):

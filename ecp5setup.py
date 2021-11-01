5709b29dd0e4edd997171913c7174f6f357f2f44
emard <vordah@gmail.com>
2021-11-01 18:18:05 +0100

    ecp5setup wifi, main, pinout

--- ecp5setup.py
+++ ecp5setup.py
@@ -41,13 +41,14 @@ def main():
   )
   f.close()
 
-yn=input("run FTP server at boot (overwrite 'main.py') (y/n)? ")
+yn=input("overwrite 'main.py' to run FTP server at boot (y/n)? ")
 if yn.startswith("y"):
   main()
 
 def pins_v20():
   f=open("jtagpin.py","w")
   f.write(
+"# ULX3S v2.x.x or v3.0.x\n"
 "tms=const(21)\n"
 "tck=const(18)\n"
 "tdi=const(23)\n"
@@ -60,6 +61,7 @@ def pins_v20():
 def pins_v31():
   f=open("jtagpin.py","w")
   f.write(
+"# ULX3S v3.1.x\n"
 "tms=const(21)\n"
 "tck=const(18)\n"
 "tdi=const(23)\n"
@@ -69,25 +71,33 @@ def pins_v31():
   )
   f.close()
 
-def pins_v31():
+def pins_fjc():
   f=open("jtagpin.py","w")
-  f.write("tms=const(5)\n")
-  f.write("tck=const(18)\n")
-  f.write("tdi=const(23)\n")
-  f.write("tdo=const(34)\n")
-  f.write("tcknc=const(21)\n")
-  f.write("led=const(19)\n")
+  f.write(
+"# FJC-ESP32-V0r2\n"
+"tms=const(4)\n"
+"tck=const(16)\n"
+"tdi=const(15)\n"
+"tdo=const(2)\n"
+"tcknc=const(21)\n"
+"led=const(19)\n"
+  )
   f.close()
+
 
 yn=input("change JTAG pinout (y/n)? ")
 if yn.startswith("y"):
   print("0: ULX3S v2.x.x or v3.0.x")
   print("1: ULX3S v3.1.x")
-  pinout=input("select pinout (0/1)> ")
+  print("2: FJC-ESP32-V0r2")
+  pinout=input("select pinout (0-2)> ")
   if pinout.startswith("0"):
     pins_v20()
     print("selected pinout: ULX3S v2.x.x or v3.0.x")
   if pinout.startswith("1"):
     pins_v31()
     print("selected pinout: ULX3S v3.1.x")
+  if pinout.startswith("2"):
+    pins_fjc()
+    print("selected pinout: FJC-ESP32-V0r2")
   printfile("jtagpin.py")

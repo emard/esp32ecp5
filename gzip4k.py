cb5d3721d38acdfb8e0cb3f82267b7172482a939
emard <vordah@gmail.com>
2021-11-03 20:37:26 +0100

    gzip 4K tool for compressing bitstream to .gz file. Such .gz file is suitable for uncompressing with small RAM usage on ESP32 micropython.

---
+++ gzip4k.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# gzip-compress file using small block size 4K,
+# suitable for unzipping at devices with small RAM,
+# micropython friendly
+
+import os, sys, zlib
+
+def gzip4k(fname):
+  stream = open(fname, "rb")
+  comp = zlib.compressobj(level=9, wbits=16 + 12)
+  with open(fname + ".gz", "wb") as outf:
+    while 1:
+      data = stream.read(1024)
+      if not data:
+        break
+      outf.write(comp.compress(data))
+    outf.write(comp.flush())
+  os.remove(fname)
+
+if __name__ == "__main__":
+  gzip4k(sys.argv[1])

1fe1447517c794cb8ab91a7ba931228509510a59
emard <vordah@gmail.com>
2021-12-01 14:15:34 +0100

    updated boot.py main.py

---
+++ boot.py
@@ -0,0 +1,6 @@
+# boot.py: executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import webrepl
+webrepl.start()
+gc.collect()
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+# main.py: executed on every boot
 try:
   import wifiman
 except:
-  print("wifiman.py error")
+  print('wifiman.py error')
 import gc
 gc.collect()
 import uftpd
 from ntptime import settime
 try:
   settime()
+  print('NTP time set')
 except:
-  print("NTP not available")
+  print('NTP not available')

686b832e6b662854395e5bac3370855c14e07794
emard <vordah@gmail.com>
2025-05-26 09:04:37 +0200

    install ptp.py to /lib

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ $MPREMOTE connect $PORT cp ecp5wp.py :/lib/ecp5wp.py
 $MPREMOTE connect $PORT cp ecp5setup.py :/lib/ecp5setup.py
 $MPREMOTE connect $PORT cp wifiman.py :/lib/wifiman.py
 $MPREMOTE connect $PORT cp dfu.py :/lib/dfu.py
+$MPREMOTE connect $PORT cp ptp.py :/lib/ptp.py
 $MPREMOTE connect $PORT cp wifiman.conf :/wifiman.conf
 $MPREMOTE connect $PORT cp jtagpin.py :/jtagpin.py
 $MPREMOTE connect $PORT cp sdpin.py :/sdpin.py

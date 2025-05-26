d90ed50eca736a491d9d08793a99faa3ae9ab01b
emard <vordah@gmail.com>
2025-05-26 16:57:24 +0200

    install.sh move lines around to be more tidy

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ $MPREMOTE connect $PORT cp uftpd.py :/lib/uftpd.py
 $MPREMOTE connect $PORT cp sdraw.py :/lib/sdraw.py
 $MPREMOTE connect $PORT cp ecp5wp.py :/lib/ecp5wp.py
 $MPREMOTE connect $PORT cp ecp5setup.py :/lib/ecp5setup.py
-$MPREMOTE connect $PORT cp wifiman.py :/lib/wifiman.py
 $MPREMOTE connect $PORT cp dfu.py :/lib/dfu.py
 $MPREMOTE connect $PORT cp ptp.py :/lib/ptp.py
+$MPREMOTE connect $PORT cp wifiman.py :/lib/wifiman.py
 $MPREMOTE connect $PORT cp wifiman.conf :/wifiman.conf
 $MPREMOTE connect $PORT cp jtagpin.py :/jtagpin.py
 $MPREMOTE connect $PORT cp sdpin.py :/sdpin.py

ab167d2cfe8f789b23017a7478bf29b34851602a
emard <vordah@gmail.com>
2025-05-26 08:02:37 +0200

    install script using mpremote

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PORT=/dev/ttyACM0
+MPREMOTE=~/.local/bin/mpremote
+
+$MPREMOTE connect $PORT mkdir :/lib
+$MPREMOTE connect $PORT cp ecp5.py :/lib/ecp5.py
+$MPREMOTE connect $PORT cp uftpd.py :/lib/uftpd.py
+$MPREMOTE connect $PORT cp sdraw.py :/lib/sdraw.py
+$MPREMOTE connect $PORT cp ecp5wp.py :/lib/ecp5wp.py
+$MPREMOTE connect $PORT cp ecp5setup.py :/lib/ecp5setup.py
+$MPREMOTE connect $PORT cp wifiman.py :/lib/wifiman.py
+$MPREMOTE connect $PORT cp wifiman.conf :/wifiman.conf
+$MPREMOTE connect $PORT cp jtagpin.py :/jtagpin.py
+$MPREMOTE connect $PORT cp sdpin.py :/sdpin.py
+$MPREMOTE connect $PORT cp main.py :/main.py
